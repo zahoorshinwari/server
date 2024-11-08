9fcb1b6c16c5f9d344442e2b4affc0eaa06f61ec
zahoorshinwari <khanzshinwari5371@gmail.com>
2024-11-08 12:30:57 +0500

    customer route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT =  8000;
 
 // Get MongoDB URI from environment variables
-const MONGODB_URI = 'mongodb+srv://khanzshinwari5371:XK4nYA2ok8M1Imnz@auth.xa8qt.mongodb.net/?retryWrites=true&w=majority&appName=auth'
+const MONGODB_URI = 'mongodb+srv://khanzshinwari5371:XK4nYA2ok8M1Imnz@authentication.jhj9h.mongodb.net/?retryWrites=true&w=majority&appName=authentication'
 
 
 // Initialize MongoDB Client
@@ -14,7 +14,7 @@ async function connectToDatabase() {
   try {
     const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
-    db = client.db('auth'); 
+    db = client.db('customer'); 
     console.log('✅ Connected to MongoDB');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
@@ -32,9 +32,9 @@ app.get('/about', (req, res) => {
   res.send('About route 🎉');
 });
 
-app.get('/users', async (req, res) => {
+app.get('/customer', async (req, res) => {
   try {
-    const collection = db.collection('users'); // replace with your collection name
+    const collection = db.collection('companies'); // replace with your collection name
     const data = await collection.find().toArray();
     res.json(data);
   } catch (error) {
