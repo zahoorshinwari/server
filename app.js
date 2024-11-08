68890af4efe6607bfef25fb0af5db01241493bfb
zahoorshinwari <khanzshinwari5371@gmail.com>
2024-11-08 12:52:17 +0500

    updated

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
-const PORT =  8000;
+const PORT = 8000;
 
 const cors = require('cors');
 
-
 // Get MongoDB URI from environment variables
-const MONGODB_URI = 'mongodb+srv://khanzshinwari5371:XK4nYA2ok8M1Imnz@authentication.jhj9h.mongodb.net/?retryWrites=true&w=majority&appName=authentication'
-
+const MONGODB_URI = 'mongodb+srv://khanzshinwari5371:XK4nYA2ok8M1Imnz@authentication.jhj9h.mongodb.net/?retryWrites=true&w=majority&appName=authentication';
 
 // Initialize MongoDB Client
 let db;
@@ -17,7 +15,7 @@ async function connectToDatabase() {
   try {
     const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect();
-    db = client.db('customer'); 
+    db = client.db('customer');
     console.log('✅ Connected to MongoDB');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
@@ -29,7 +27,6 @@ connectToDatabase();
 
 app.use(cors());
 
-
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
@@ -39,19 +36,16 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/customer', async (req, res) => {
-    try {
-      const db = await connectToDatabase(); // Ensure db connection
-      const collection = db.collection('companies');
-      const data = await collection.find().toArray();
-      res.json(data);
-    } catch (error) {
-      console.error('Error fetching data:', error.message);
-      console.error(error.stack); // Log the full stack trace
-      res.status(500).send('Error fetching data');
-    }
-  });
-  
-  
+  try {
+    const collection = db.collection('companies'); // Access the `db` directly
+    const data = await collection.find().toArray();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    console.error(error.stack); // Log the full stack trace
+    res.status(500).send('Error fetching data');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
